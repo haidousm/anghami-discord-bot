dfbc0ab65f51adea41766108760cce0a4f2b30bd
haidousm <haidous.m@gmail.com>
2021-03-17 22:30:04 +0200

    add alternative replies if im using the bot

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ client.on("message", async (message) => {
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
 
+    let isMoussa = false;
+
+    if (message.member.id == "5003") {
+        isMoussa = true;
+    }
+
     const voiceChannel = message.member.voice.channel;
     if (!voiceChannel) return message.channel.send("join the channel dumbo");
 
@@ -47,9 +53,15 @@ client.on("message", async (message) => {
     if (message.content.startsWith(`${prefix}play`)) {
         if (serverQueue.player !== null) {
             serverQueue.player.resume();
-            message.channel.send("ok im resuming");
+            if (isMoussa) {
+                message.channel.send(
+                    "has anyone ever told you that you have an amazing music taste??"
+                );
+            } else {
+                message.channel.send("ok im resuming");
+            }
         } else {
-            playSongs(message, serverQueue);
+            playSongs(message, serverQueue, isMoussa);
         }
         return;
     } else if (
@@ -57,25 +69,47 @@ client.on("message", async (message) => {
         message.content.startsWith(`${prefix}s`) ||
         message.content.startsWith(`${prefix}fs`)
     ) {
-        message.channel.send("thank fuck i couldnt bare it anymore");
+        if (isMoussa) {
+            message.channel.send(
+                "im looking forward to the next song cause youre music taste is amazing!"
+            );
+        } else {
+            message.channel.send("thank fuck i couldnt bare it anymore");
+        }
         skip(message, serverQueue);
         return;
     } else if (
         message.content.startsWith(`${prefix}pause`) ||
         message.content.startsWith(`${prefix}p`)
     ) {
-        message.channel.send("ugh ok i paused");
+        if (isMoussa) {
+            message.channel.send("as you wish <3");
+        } else {
+            message.channel.send("ugh ok i paused");
+        }
+
         serverQueue.player.pause();
         return;
     } else if (message.content.startsWith(`${prefix}stop`)) {
-        message.channel.send("bye bitch");
+        if (isMoussa) {
+            message.channel.send("gonna miss you !!");
+        } else {
+            message.channel.send("bye bitch");
+        }
+
         stop(message, serverQueue);
         return;
     } else if (
         message.content.startsWith(`${prefix}queue`) ||
         message.content.startsWith(`${prefix}q`)
     ) {
-        let queue = "here's your shit song list:\n";
+        let queue = "";
+        if (isMoussa) {
+            queue = message.channel.send("here's the best song list ever:\n");
+        } else {
+            queue = "here's your shit song list:\n";
+        }
+
         let i = 1;
         for (const song of serverQueue.songs) {
             queue += `${i} - ${song.title}\n`;
@@ -84,7 +118,14 @@ client.on("message", async (message) => {
         message.channel.send(queue);
         return;
     } else if (message.content.startsWith(`${prefix}shuffle`)) {
-        message.channel.send("no shuffling will fix your ass music taste");
+        if (isMoussa) {
+            message.channel.send(
+                "im super excited to see what song ill get to play for you<3:\n"
+            );
+        } else {
+            message.channel.send("no shuffling will fix your ass music taste");
+        }
+
         serverQueue.songs = shuffle(serverQueue.songs);
         return;
     } else {
@@ -92,7 +133,7 @@ client.on("message", async (message) => {
     }
 });
 
-async function playSongs(message, serverQueue) {
+async function playSongs(message, serverQueue, isMoussa) {
     let command = message.content;
     let userUrl = command.substr(command.indexOf(" ") + 1);
 
@@ -110,7 +151,7 @@ async function playSongs(message, serverQueue) {
     if (serverQueue.songs.length == 0 && serverQueue.playing == false) {
         let firstSong = await parseRawSong(rawSongs.shift());
         try {
-            play(message.guild, firstSong);
+            play(message.guild, firstSong, isMoussa);
             serverQueue.playing = true;
         } catch (err) {
             console.log(err);
@@ -154,7 +195,7 @@ function stop(message, serverQueue) {
     serverQueue.connection.dispatcher.end();
 }
 
-function play(guild, song) {
+function play(guild, song, isMoussa) {
     const serverQueue = queue.get(guild.id);
     if (!song) {
         serverQueue.voiceChannel.leave();
@@ -165,11 +206,12 @@ function play(guild, song) {
     const dispatcher = serverQueue.connection
         .play(ytdl(song.url))
         .on("finish", () => {
-            play(guild, serverQueue.songs.shift());
+            play(guild, serverQueue.songs.shift(), isMoussa);
         })
         .on("error", (error) => console.error(error));
     serverQueue.player = dispatcher;
     dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+
     serverQueue.textChannel.send(`im playing this shit song **${song.title}**`);
 }
 
